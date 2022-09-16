b70d6bf695522d0b408cfb37442ee9b33c75aa0d
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-15 22:42:54 -0400

    Create minmax.cpp

---
+++ minmax.cpp
@@ -0,0 +1,48 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 3B
+This code writes a program that finds the minimum and maximum storage in East basin in 2018.
+*/
+
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<climits>
+using namespace std;
+
+int main()
+{
+string date;
+double eastSt,eastEl, westSt,westEl, min, max;
+ifstream fin("Current_Reservoir_Levels.tsv");
+if(fin.fail())
+{
+cerr << "File cannot be opened for reading." << endl;
+exit(1); 
+}
+string junk; 
+getline(fin, junk); 
+int i=0;
+while(fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+{
+fin.ignore(INT_MAX, '\n'); 
+
+if(i==0)
+{
+min = eastSt;
+max = eastSt;
+}
+if(min>eastSt)
+min = eastSt;
+if(max<eastSt)
+max = eastSt;
+i++;
+}
+cout<<"minimum storage in East basin: "<<min<<" billion gallons"<<endl;
+cout<<"MAXimum storage in East basin: "<<max<<" billion gallons"<<endl;
+fin.close();
+
+  return 0;
+}
