4d23d7033e88d08345e0a109b9bc26c5c2581ed6
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-15 22:35:08 -0400

    Update reservoir.cpp

--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,24 @@
-// add your code
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 3A
+Notes: I dont understand how to do this.I was wondering if you could retell me your office hours or maube we can set up an appointment because i think i 
+can really benfit from talking to you one-on-one.
+*/
+
+#include<iostream>
+#include<fstream>
+#include <cstdlib>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+  
+double get_east_storage(std::string date)
+
+ return 0;
+
+}
