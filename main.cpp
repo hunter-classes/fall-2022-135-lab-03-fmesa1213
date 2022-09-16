de74e8c5bd5d729ed9b7475d24ef243573433beb
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-15 22:18:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
-// add your code as appropriate 
-#include <iostream>
+
+#include<iostream>
+#include<fstream>
+#include <cstdlib>
+#include <climits>
+
+using namespace std;
 
 int main()
 {
-  
 
-  
-  return 0;
+string date;
+double eastSt,eastEl,westSt,westEl;
+
+string userdate;  
+cout<<"Enter date: ";
+cin>>userdate;
+
+ifstream fin("Current_Reservoir_Levels.tsv");
+
+if (fin.fail()) 
+{
+cerr << "File cannot be opened for reading." << endl;
+exit(1); 
+}
+
+string junk; 
+getline(fin, junk);
+while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) 
+{
+fin.ignore(INT_MAX, '\n'); 
+
+if(date==userdate)
+{
+cout<<"East basin storage: "<<eastSt<<" gallons"<<endl;
+break;
+}
+}
+
+fin.close();
+return 0;
+
 }
